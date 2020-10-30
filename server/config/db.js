946309e5710960abebcfee7d554fca8ad605a7b7
kitsabado <ksabado@my.centennialcollege.ca>
2020-10-30 15:15:30 -0400

    updated URI to work with mongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://kristopher:NllYGv0PBYeWv5TS@mongodbserver.u2zux.mongodb.net/books229?retryWrites=true&w=majority"
 };
